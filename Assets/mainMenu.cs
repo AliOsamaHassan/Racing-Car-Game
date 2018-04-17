05199f0880d2317a397663e0582a384860809b11
abdulrahman <abdooawad43@gmail.com>
2018-04-17 21:20:57 +0200

    start game menu

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+	public void playGame()
+    {
+        SceneManager.LoadScene("RaceArea01");
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
