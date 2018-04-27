f7bf69a53fc3013107715eb4c24b7f99cb60ced4
abdulrahman <abdooawad43@gmail.com>
2018-04-27 19:25:29 +0200

    menu & enviroments

---
+++ Assets/Enviroment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviroment : MonoBehaviour {
+
+    private string scene = "";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void  onSceneSelect(string scene)
+    {
+        
+        this.scene = scene;  
+    }
+    public void select()
+    {
+        mainMenue.enviroment = scene;
+    }
+}
