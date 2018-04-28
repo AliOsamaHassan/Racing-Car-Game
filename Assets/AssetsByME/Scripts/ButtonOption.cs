388d2ac424e5b66fa546759a1a11fd1de8283785
Ahmed Shaaban <ahmedshaaban837@gmail.com>
2018-04-28 19:55:22 +0200

    add skybox ,main menu , cameras views,music, car and track selection

---
+++ Assets/AssetsByME/Scripts/ButtonOption.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOption : MonoBehaviour {
+    public void playGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void TrackSelect()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void quitGame()
+    {
+        // if (Input.GetKey("escape"))
+        Application.Quit();
+        
+    }
+
+    ///
+    public void track01()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void track02()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
--- Assets/AssetsByME/Scripts/LapComplete.cs
+++ Assets/AssetsByME/Scripts/LapComplete.cs
@@ -14,10 +14,12 @@ public class LapComplete : MonoBehaviour
     public GameObject MilliDisplay;
 
     public GameObject LapTimeBox;
-
+    // count the laps that was compeleted
+    public GameObject LapCounter;
+    public int LapsDone;
     void OnTriggerEnter()
     {
-
+        LapsDone += 1;
         if (LapTimeManager.SecondCount <= 9)
         {
             SecondDisplay.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
@@ -38,9 +40,16 @@ public class LapComplete : MonoBehaviour
 
         MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
 
+        // added by Ahmed Shaaban
+        PlayerPrefs.SetInt("MinSave",LapTimeManager.MinuteCount);
+        PlayerPrefs.SetInt("SecSave",LapTimeManager.SecondCount);
+        PlayerPrefs.SetFloat("MilliSave",LapTimeManager.MilliCount);
+
         LapTimeManager.MinuteCount = 0;
         LapTimeManager.SecondCount = 0;
         LapTimeManager.MilliCount = 0;
+        ///
+        LapCounter.GetComponent<Text>().text = "" + LapsDone;
 
         HalfLapTrig.SetActive(true);
         LapCompleteTrig.SetActive(false);
